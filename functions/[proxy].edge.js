173158afd9c18b6897594e6e99b2b0218888cd30
Kelly Brennan <kelly_brennan@epam.com>
2024-11-05 12:23:03 -0500

    debugging

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -16,14 +16,5 @@ export default async function handler(request, context) {
   await Personalize.init(context.env.NEXT_PUBLIC_CONTENTSTACK_PERSONALIZE_PROJECT_UID, {
     request,
   });
-  const variantParam = Personalize.getVariantParam();
-  parsedUrl.searchParams.set(Personalize.VARIANT_QUERY_PARAM, variantParam);
-  const modifiedRequest = new Request(parsedUrl.toString(), request);
-  const response = await fetch(modifiedRequest);
-  const modifiedResponse = new Response(response.body, response);
-  // add cookies to the response
-  await Personalize.addStateToResponse(modifiedResponse);
-  // ensure that the response is not cached on the browser
-  modifiedResponse.headers.set('cache-control', 'no-store');
-  return modifiedResponse;
+
 }
