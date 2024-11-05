c742dbec3fafa87166acfb640ee9348a7b8d8145
Kelly Brennan <kelly_brennan@epam.com>
2024-11-05 15:58:01 -0500

    Exclude Asset Calls

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,17 +1,26 @@
 import Personalize from '@contentstack/personalize-edge-sdk';
 export default async function handler(request, context) {
   const parsedUrl = new URL(request.url);
+  const pathname = parsedUrl.pathname;
+  // exclude Next.js asset calls so that only page requests are processed
+  if (['_next', 'favicon.ico'].some((path) => pathname.includes(path))) {
+    return fetch(request);
+  }
+  // reset the SDK to remove any existing context
   Personalize.reset();
+  // set a custom edge API URL
   if (context.env.NEXT_PUBLIC_CONTENTSTACK_PERSONALIZE_EDGE_API_URL) {
     Personalize.setEdgeApiUrl(context.env.NEXT_PUBLIC_CONTENTSTACK_PERSONALIZE_EDGE_API_URL);
   }
+  // Initialize the SDK and pass the request as well
   await Personalize.init(context.env.NEXT_PUBLIC_CONTENTSTACK_PERSONALIZE_PROJECT_UID, {
     request,
   });
   const variantParam = Personalize.getVariantParam();
-  if (variantParam) {
+    if (variantParam) {
 	parsedUrl.searchParams.set(Personalize.VARIANT_QUERY_PARAM, variantParam);
   }
+  parsedUrl.searchParams.set(Personalize.VARIANT_QUERY_PARAM, variantParam);
   const modifiedRequest = new Request(parsedUrl.toString(), request);
   const response = await fetch(modifiedRequest);
   const modifiedResponse = new Response(response.body, response);
