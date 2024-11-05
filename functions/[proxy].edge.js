bfc52ac888c5c13e6f1fe49c06fa1900961a577f
Kelly Brennan <kelly_brennan@epam.com>
2024-11-05 15:15:05 -0500

    debugging

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,20 +1,3 @@
-import Personalize from '@contentstack/personalize-edge-sdk';
 export default async function handler(request, context) {
-  const parsedUrl = new URL(request.url);
-  const pathname = parsedUrl.pathname;
-  // exclude Next.js asset calls so that only page requests are processed
-  if (['_next', 'favicon.ico'].some((path) => pathname.includes(path))) {
-    return fetch(request);
-  }
-  // reset the SDK to remove any existing context
-  Personalize.reset();
-  // set a custom edge API URL
-  if (context.env.NEXT_PUBLIC_CONTENTSTACK_PERSONALIZE_EDGE_API_URL) {
-    Personalize.setEdgeApiUrl(context.env.NEXT_PUBLIC_CONTENTSTACK_PERSONALIZE_EDGE_API_URL);
-  }
-  // Initialize the SDK and pass the request as well
-  await Personalize.init(context.env.NEXT_PUBLIC_CONTENTSTACK_PERSONALIZE_PROJECT_UID, {
-    request,
-  });
-
+  return fetch(request);
 }
