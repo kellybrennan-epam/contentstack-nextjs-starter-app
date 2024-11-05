9aca8de563168456eed638a26e13c0cfe1c9d33e
Kelly Brennan <kelly_brennan@epam.com>
2024-11-05 15:22:05 -0500

    Debugging edge functions

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,3 +1,12 @@
+import Personalize from '@contentstack/personalize-edge-sdk';
 export default async function handler(request, context) {
+  // set a custom edge API URL
+  if (context.env.NEXT_PUBLIC_CONTENTSTACK_PERSONALIZE_EDGE_API_URL) {
+    Personalize.setEdgeApiUrl(context.env.NEXT_PUBLIC_CONTENTSTACK_PERSONALIZE_EDGE_API_URL);
+  }
+  // Initialize the SDK and pass the request as well
+  await Personalize.init(context.env.NEXT_PUBLIC_CONTENTSTACK_PERSONALIZE_PROJECT_UID, {
+    request,
+  });
   return fetch(request);
 }
