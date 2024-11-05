5df37e6123ba1df72d1062a869db232c57a98c69
Kelly Brennan <kelly_brennan@epam.com>
2024-11-05 15:34:18 -0500

    Debugging ege function - Personalize.reset()

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -3,18 +3,13 @@ export default async function handler(request, context) {
   const parsedUrl = new URL(request.url);
   // reset the SDK to remove any existing context
   Personalize.reset();
+  // set a custom edge API URL
   if (context.env.NEXT_PUBLIC_CONTENTSTACK_PERSONALIZE_EDGE_API_URL) {
     Personalize.setEdgeApiUrl(context.env.NEXT_PUBLIC_CONTENTSTACK_PERSONALIZE_EDGE_API_URL);
   }
+  // Initialize the SDK and pass the request as well
   await Personalize.init(context.env.NEXT_PUBLIC_CONTENTSTACK_PERSONALIZE_PROJECT_UID, {
     request,
   });
-  // get the variant parameter from the SDK
-  const variantParam = Personalize.getVariantParam();
-  // set the variant parameter as a query param in the URL
-  parsedUrl.searchParams.set(Personalize.VARIANT_QUERY_PARAM, variantParam);
-  // rewrite the request with the modified URL
-  const modifiedRequest = new Request(parsedUrl.toString(), request);
-  const response = await fetch(modifiedRequest);
-  return response;
+  return fetch(request);
 }
